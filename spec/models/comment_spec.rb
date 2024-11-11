42b84bff037efdc1308752ce8e8fc90fe3aac60a
sumitpati7 <sumitpati7@gmail.com>
2024-11-11 14:23:30 +0545

    feat: annotated model view

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  user_id    :bigint           not null
+#  product_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
