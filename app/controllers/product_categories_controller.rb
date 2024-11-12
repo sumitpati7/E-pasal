1c7bcddec9e125b3a32d9b0e4a9e4bec653f19c7
HabiPyatha <unionhab@gmail.com>
2024-11-12 19:24:19 +0545

    product category added and drop down also correct

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -3,6 +3,16 @@ class ProductCategoriesController < ApplicationController
     @category=ProductCategory.new
   end
   def create
-    
+    @category=ProductCategory.new(category_params)
+    if @category.save
+      redirect_to root_path, notice: "A Category was added sucessfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def category_params
+    params.require(:product_category).permit(:name)
   end
 end
