0bd2b8c4af0512fee2e6bfb69f0b24e59f90c00b
sumitpati7 <sumitpati7@gmail.com>
2024-11-14 14:27:33 +0545

    feat: turbo create and edit form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,26 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: "Product updated successfully!" }
+        format.turbo_stream
+      end
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: "Product successfully destroyed!" }
+        format.turbo_stream
+      end
+    end
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :brand, :description, :price, :product_category_id, :stock, :vendor_id, :discount_percentage, product_image:[])
