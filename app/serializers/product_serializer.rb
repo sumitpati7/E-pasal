26fd244f59a43401e2defa746d14d223c4e527e3
sumitpati7 <sumitpati7@gmail.com>
2024-11-18 12:26:51 +0545

    feat: product_serializer to include

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -16,6 +16,7 @@
 #
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :brand, :image_urls, :description, :price, :stock, :discount_percentage, :product_category_id, :vendor_id, :created_at, :updated_at
-
+  belongs_to :product_category
+  has_many :comments
   
 end
