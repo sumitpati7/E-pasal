32fc4d8cd016861c654acee10654c86d1e642885
HabiPyatha <unionhab@gmail.com>
2024-11-19 22:38:12 +0545

    name chnage in products

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -15,8 +15,17 @@
 #  vendor_id           :bigint           not null
 #
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :brand, :image_urls, :description, :price, :stock, :discount_percentage, :product_category_id, :vendor_id, :created_at, :updated_at
+  # attributes :id, :name, :brand, :image_urls, :description, :price, :stock, :discount_percentage, :product_category_id, :vendor_id, :created_at, :updated_at
+  attributes :id, :title, :brand, :images, :description, :price, :stock, :discount_percentage, :product_category_id, :vendor_id, :created_at, :updated_at
   belongs_to :product_category
   has_many :comments
   belongs_to :vendor
+
+  def title
+    object.name
+  end
+
+  def images
+    object.image_urls
+  end
 end
