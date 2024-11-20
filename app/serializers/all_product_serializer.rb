39cf515acf2754e612cca2378a813232a7127379
sumitpati7 <sumitpati7@gmail.com>
2024-11-20 12:58:34 +0545

    feat: discount_percentage for all products

--- app/serializers/all_product_serializer.rb
+++ app/serializers/all_product_serializer.rb
@@ -1,3 +1,3 @@
 class AllProductSerializer < ActiveModel::Serializer
-    attributes :id, :name, :image_urls, :brand, :price
+    attributes :id, :name, :image_urls, :brand, :price, :discount_percentage
   end
---
+++ db/migrate/20241120070326_add_image_url_to_product.rb
@@ -0,0 +1,5 @@
+class AddImageUrlToProduct < ActiveRecord::Migration[7.2]
+  def change
+    add_column :products, :image_url, :string
+  end
+end
