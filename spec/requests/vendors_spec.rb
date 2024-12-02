cbd953549c55017a7617f4de54fddd68c562a23f
ShirajShrestha <shresthashiraj333@gmail.com>
2024-12-02 16:34:45 +0000

    Add test code for vendor controller of api

--- spec/requests/vendors_spec.rb
+++ spec/requests/vendors_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Vendors", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /show" do
+    let(:vendor) {
+      Vendor.create!(email: "abc@gmail.com", password: "1234abcd", owner_name: "ven", shop_name: "dorshop", address: "ven dor", contact_number: "9876543210")
+    }
+
+    it "returns a list of products added by that particular vendor" do
+      get "/api/v1/vendors/#{vendor.id}"
+      expect(response).to have_http_status(:ok) # expecting valid status code
+      # parsed_response = JSON.parse(response.body)
+      # expect(parsed_response["message"]).to eq("Vendor found")
+    end
+
+    it "returns an error when vendor is not found" do
+      get "/api/v1/vendors/111"
+      expect(response).to have_http_status(:not_found)
+      # Somehow the code below doesn't work
+      # parsed_response = JSON.parse(response.body)
+      # expect(parsed_response["messsage"]).to eq("Vendor not found")
+    end
   end
 end
