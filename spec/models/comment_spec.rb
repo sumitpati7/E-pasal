08d1b50d08e0b402c59e205f981831cc0502a41d
ShirajShrestha <shresthashiraj333@gmail.com>
2024-12-02 10:18:55 +0000

    Add order and comment test cases

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,4 +12,86 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:category) {
+    ProductCategory.create!(name: "Powerbank")
+  }
+
+  let(:vendor) {
+    Vendor.create!(
+      email: "habi2222@gmail.com",
+      password: "123456789",
+      owner_name: "habi",
+      shop_name: "Pro",
+      address: "thimi",
+      contact_number: "9876543211")
+    }
+
+  let(:user) {
+    User.create!(
+      first_name: "Sanford",
+      middle_name: "Mc",
+      last_name: "Gregor",
+      email: "sanford@mcgregor.com",
+      password: "sanfordmcgregor",
+      address: "somewhere",
+      contact_phone: "9876543210",
+    )
+  }
+
+  let(:product) {
+    Product.create!(
+      title: "sunsilk shampoo",
+      brand: "Sunsilk",
+      description: "Hello World",
+      price: 350,
+      stock: 12,
+      discount_percentage: 12,
+      product_category_id: category.id,
+      vendor_id: vendor.id
+    )
+  }
+
+  let(:valid_comment) {
+    Comment.new(
+      content: "Great product!",
+      user_id: user.id,
+      product_id: product.id
+    )
+  }
+
+  # associations
+  describe 'associations' do
+    it "belongs to a user" do
+      expect(valid_comment.user).to eq(user)
+    end
+
+    it "belongs to a product" do
+      expect(valid_comment.product).to eq(product)
+    end
+  end
+
+  # validations
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(valid_comment).to be_valid
+    end
+
+    it "is invalid without user" do
+      valid_comment.user = nil
+      valid_comment.valid?
+      expect(valid_comment.errors[:user]).to include("must exist")
+    end
+
+    it "is invalid without product" do
+      valid_comment.product = nil
+      valid_comment.valid?
+      expect(valid_comment.errors[:product]).to include("must exist")
+    end
+
+    it "is invalid without a content" do
+      valid_comment.content = nil
+      valid_comment.valid?
+      expect(valid_comment.errors[:content]).to include("can't be blank")
+    end
+  end
 end
