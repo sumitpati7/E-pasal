7b8562e01805b40313c1ecac693edc37cd1867cb
DikuCoding <dikeshkhagi1@gmail.com>
2024-12-03 16:05:57 +0545

    add helper to display image of product

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
-end
+    def ProductImage(product, css)
+       if product.product_image.attached?
+        image_tag url_for(product.product_image), class: "#{css}", alt: product.title
+       else
+        # image_tag src="product.image_url" alt="product.title" class="w-10 h-10 rounded mr-4"
+        image_tag "#{product.image_url}", class: "#{css}"
+       end
+    end
+end 
+# class: "w-10 h-10 rounded mr-4"
