baa45f9efda1cdafc657d6bae973290aedbe5e00
DikuCoding <dikeshkhagi1@gmail.com>
2024-12-03 16:47:34 +0545

    remove unnecessary lines

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -6,9 +6,7 @@ module ProductsHelper
        if product.product_image.attached?
         image_tag url_for(product.product_image), class: "#{css}", alt: product.title
        else
-        # image_tag src="product.image_url" alt="product.title" class="w-10 h-10 rounded mr-4"
-        image_tag "#{product.image_url}", class: "#{css}"
+        image_tag "#{product.image_url}", alt: "#{product.title}", class: "#{css}"
        end
     end
-end 
-# class: "w-10 h-10 rounded mr-4"
+end
