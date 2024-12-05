ef1f846145bf80b7f76f60be7afa17b96ce828ff
sumitpati7 <sumitpati7@gmail.com>
2024-12-05 16:52:38 +0545

    show discounted price change

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,6 +3,19 @@ module ProductsHelper
     number_with_precision(product.price.to_f - ((product.discount_percentage.to_f / 100.0)*product.price.to_f), precision: 2)
   end
 
+  def show_price(product)
+    if product.discount_percentage>0
+      content_tag(:div, class: "flex justify-center gap-1 flex-col") do
+        content_tag(:span, calc_discounted_amount(product), class: "text-green-600 w-full text-center text-[24px]")+
+        content_tag(:span, class: "flex gap-1 justify-center") do
+          content_tag(:span, product.price, class: "text-red-600 line-through") + content_tag(:span, "#{product.discount_percentage}% off", class: "text-red-800")
+        end
+      end
+    else
+      content_tag(:div, product.price, class: "text-green-600 text-center")
+    end
+  end
+
   def ProductImage(product, css)
       if product.product_image.attached?
       image_tag url_for(product.product_image), class: "#{css}", alt: product.title, title: product.title
