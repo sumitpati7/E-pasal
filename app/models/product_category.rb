db4e93f8a8686e2e20d8ab0cf63d1fa9435e16bd
HabiPyatha <unionhab@gmail.com>
2024-12-06 13:26:53 +0545

    add category place changed.

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -11,4 +11,7 @@
 class ProductCategory < ApplicationRecord
   has_many :products, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  after_create_commit -> { broadcast_append_to "category", partial: "shared/category_name", locals: { category: self }, target: "category" }
 end
+# after_update_commit -> { broadcast_replace_to "products", partial: "shared/product", locals: { product: self } }
+# after_destroy_commit -> { broadcast_remove_to "products" }
